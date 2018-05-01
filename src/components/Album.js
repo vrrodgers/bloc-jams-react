9136f47b9ae2eada0a638d5398b2bc11b17fd897
vrrodgers <trulymissvicky1982@gmail.com>
2018-04-30 19:30:40 -0500

    updated album components

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,30 +9,41 @@ class Album extends Component {
         this.state = {
             album: album
         };
+
+      
     }
   render() {
-    return (
-      <section className="album">
-            <section id="album-info">
-                <img id="album-cover-art" src={this.state.album.albumCover} />
-                <div className="album-details">
-                    <h1 id="album-title">{this.state.album.title}</h1>
-                    <h2 className="artist">{this.state.album.artist}</h2>
-                    <div id="release-info">{this.state.album.releaseInfo}</div>
+    return <section className="album">
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody className="song-list">
+            
+                {this.state.album.songs.map((album, index) =>
+                    <div key={index}> 
+                    <div>{index + 1} </div> 
+                    <div> {album.index}</div> 
+                    <div>{album.title}</div> 
+                    <div>{album.duration}seconds</div>
                 </div>
-            </section>
-            <table id="song-list">
-              <colgroup>
-                <col id="song-number-column" />
-                <col id="song-title-column" />
-                <col id="song-duration-column" />
-              </colgroup>
-              <tbody>
-                  
-              </tbody>
-            </table>
-       </section>
-    );
+                
+                )
+             }
+            
+          </tbody>
+        </table>
+      </section>;
   }
 
 }
