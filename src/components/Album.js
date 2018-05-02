d2a766976e1186dc9b2b52594524fe463e2f1416
vrrodgers <trulymissvicky1982@gmail.com>
2018-05-02 10:13:11 -0500

    added player bar buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 class Album extends Component {
   constructor(props) {
     super(props);
@@ -37,6 +38,13 @@ class Album extends Component {
             this.play();
         }
     }
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
  
   render() {
     return <section className="album">
@@ -70,6 +78,12 @@ class Album extends Component {
             ))}
           </tbody>
         </table>
+        <PlayerBar 
+          isPlaying={ this.state.isPlaying} 
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+          />
       </section>;
   }
 }
